bd12fd9e3fdd4f5debb9ec0cd86b31413973b63f
Amro <ak2-hassanmahm>
2023-05-01 15:35:45 +0100

    optimised neural network using regularisation methods and set it up for training

--- models/model.py
+++ models/model.py
@@ -1,5 +1,6 @@
 from tensorflow import keras
-from keras.layers import Conv2D, Flatten, GlobalAveragePooling2D, MaxPooling2D, Input, Dense
+from keras.layers import Conv2D, Flatten, GlobalAveragePooling2D, MaxPooling2D, Input, Dense, LeakyReLU, BatchNormalization, Dropout
+from keras.regularizers import l2
 from keras.models import Model
 
 INPUT_SIZE= (224, 224, 3)
@@ -31,30 +32,39 @@ DARKNET19_ARCHITECTURE = [
     (1024, 3, 1, "same")
 ]
 
-def make_model(architecture, input_shape, num_of_classes=2):
+def build_model(architecture, input_shape, num_of_classes=2):
 
     inputs = Input(input_shape)
     x = None
 
     for c, layer in enumerate(architecture):
         if(c==0):
-            x = Conv2D(filters=layer[0], kernel_size=layer[1], strides=layer[2], padding=layer[3], activation="relu")(inputs)
+            x = Conv2D(filters=layer[0], kernel_size=layer[1], strides=layer[2], padding=layer[3], use_bias=False, kernel_regularizer=l2(0.01))(inputs)
+            x = LeakyReLU(alpha=0.2)(x)
+            x = BatchNormalization()(x)
             continue
             
         if(type(layer) == tuple):
-            x = Conv2D(filters=layer[0], kernel_size=layer[1], strides=layer[2], padding=layer[3], activation="relu")(x)
+            x = Conv2D(filters=layer[0], kernel_size=layer[1], strides=layer[2], padding=layer[3], use_bias=False, kernel_regularizer=l2(0.01))(x)
+            x = LeakyReLU(alpha=0.2)(x)
+            x = BatchNormalization()(x)
 
         if(layer == "MP"):
             x = MaxPooling2D(pool_size=(2, 2), strides=2)(x)
         
-    label = Dense(num_of_classes, activation="sigmoid", name="label")(x)
+
+    x = GlobalAveragePooling2D()(x)
+    x = Dropout(0.5)(x)
+
+    bbox = Dense(4, activation="linear", name="bbox")(x)
+    label = Dense(1, activation="sigmoid", name="label")(x)
     
-    model = Model([inputs], [label])
+    model = Model([inputs], [bbox, label])
     return model
 
 
 if(__name__ == "__main__"):
-    model = make_model(DARKNET19_ARCHITECTURE, INPUT_SIZE)
+    model = build_model(DARKNET19_ARCHITECTURE, INPUT_SIZE)
     model.build(INPUT_SIZE)
     model.summary()
 
